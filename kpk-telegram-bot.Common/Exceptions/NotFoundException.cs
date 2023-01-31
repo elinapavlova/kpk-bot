a1d59e21102bf1c4918559d356972cebe9998485
epavlova <epavlova@mmtr.ru>
2023-01-31 18:03:31 +0300

    Add NotFound exception

---
+++ kpk-telegram-bot.Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,24 @@
+﻿namespace kpk_telegram_bot.Common.Exceptions;
+
+public class NotFoundException : BaseException
+{
+    public NotFoundException(string message, string type) : base(message, type)
+    {
+    }
+
+    public NotFoundException(string message, string type, Exception ex) : base(message, type, ex)
+    {
+    }
+
+    public NotFoundException(string message, string type, Dictionary<string, string>? details) : base(message, type, details)
+    {
+    }
+
+    public NotFoundException(string message) : base(message)
+    {
+    }
+
+    public NotFoundException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
