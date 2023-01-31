aaccc2670206740fe97c374f24417ca5c836d649
epavlova <epavlova@mmtr.ru>
2023-01-31 18:04:08 +0300

    Add LessonTypeMapper

---
+++ kpk-telegram-bot.Common/Mappers/LessonTypeMapper.cs
@@ -0,0 +1,19 @@
+﻿using kpk_telegram_bot.Common.Enums;
+
+namespace kpk_telegram_bot.Common.Mappers;
+
+public static class LessonTypeMapper
+{
+    public static int Map(string lessonType)
+    {
+        return lessonType switch
+        {
+            "Л" => (int) LessonType.Lecture,
+            "ПР" => (int) LessonType.Practice,
+            "ПР СР" => (int) LessonType.IndependentPractice,
+            "СР" => (int) LessonType.IndependentWork,
+            "К" => (int) LessonType.Consultation,
+            _ => (int) LessonType.Other
+        };
+    }
+}
