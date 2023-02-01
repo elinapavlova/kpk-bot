7f3082a084fb5c5d913e761429f8f4b3b6f0644d
epavlova <epavlova@mmtr.ru>
2023-02-01 16:51:49 +0300

    Add logging import

--- kpk-telegram-bot.Core/Commands/ImportCommand.cs
+++ kpk-telegram-bot.Core/Commands/ImportCommand.cs
@@ -1,6 +1,7 @@
 ﻿using kpk_telegram_bot.Common.Contracts;
 using kpk_telegram_bot.Common.Contracts.Commands;
 using kpk_telegram_bot.Common.Contracts.HttpClients;
+using kpk_telegram_bot.Common.Logger;
 using Telegram.Bot.Types;
 
 namespace kpk_telegram_bot.Core.Commands;
@@ -9,17 +10,22 @@ public class ImportCommand : ICommand
 {
     private readonly IReader _reader;
     private readonly ITelegramHttpClient _telegramHttpClient;
+    private readonly ILogger _logger;
 
-    public ImportCommand(IReader reader, ITelegramHttpClient telegramHttpClient)
+    public ImportCommand(IReader reader, ITelegramHttpClient telegramHttpClient, ILogger logger)
     {
         _reader = reader;
         _telegramHttpClient = telegramHttpClient;
+        _logger = logger;
     }
     
     public async Task Execute(Message message)
     {
         using var stream = new MemoryStream();
-        var document = await _telegramHttpClient.GetFileById(message.Document.FileId, stream);
-        await _reader.Import(stream);
+        await _telegramHttpClient.GetFileById(message.Document.FileId, stream);
+        await _reader.Import(message.Caption, stream);
+        await _telegramHttpClient.SendTextMessage(message.From.Id, "Импорт выполнен успешно");
+        _logger.Information("Выполнен импорт файла {fileId} пользователем {userId} [{username}]", 
+            message.Document.FileId, message.From.Id, message.From.Username);
     }
 }
